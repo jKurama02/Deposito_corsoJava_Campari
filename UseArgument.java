cfe1b6dc164320b772eca2ca5e84d0a2179d463b
anmedyns <anmedyns@student.42roma.it>
2025-04-28 15:06:15 +0200

    1-day

---
+++ UseArgument.java
@@ -0,0 +1,10 @@
+public class UseArgument
+{
+	public static void main(String[] args)
+	{
+		String pippo = "pippo";
+		System.out.print("Hi, ");
+		System.out.print(pippo);
+		System.out.println(". How are you?");
+	}
+}
